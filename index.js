41cdebbb4ab1702ce80f5f1b0f9447e3218e6764
oluwanifemidomi007 <nifemibabatunde247@gmail.com>
2024-09-05 12:28:44 +0100

    Built blackjack game

--- index.js
+++ index.js
@@ -1,5 +1,52 @@
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
 
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
 
-// 2. Create a variable, sum, and set it to the sum of the two cards
+let sumEl = document.getElementById("sum-el");
+
+let messageEl = document.getElementById("message-el");
+
+let cardEl = document.getElementById("card-el");
+
+let cards = [firstCard, secondCard];
+
+function startGame() {
+  renderGame();
+}
+
+function renderGame() {
+  cardEl.textContent = "Cards: ";
+  for (let i = 0; i < cards.length; i++) {
+    cardEl.textContent += cards[i] + " ";
+  }
+  sumEl.textContent = "Sum: " + sum;
+  if (sum <= 20) {
+    message = "Do you want to draw a new card😊";
+  } else if (sum === 21) {
+    message = "you've got Blackjack😎";
+    hasBlackJack = true;
+  } else {
+    message = "You are out of the game😭";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
+}
+
+function newCard() {
+  if (hasBlackJack === false && isAlive === true) {
+    let newCardNumber = getRandomCard();
+    cards.push(newCardNumber);
+    sum += newCardNumber;
+    renderGame();
+    console.log("Drawing a new card from the deck");
+  }
+}
+
+function getRandomCard() {
+  let randomNumber = Math.floor(Math.random() * 11);
+  return randomNumber;
+}
